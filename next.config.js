2ba7d7da3535855358058c5632f326082ec9757e
Tom-BUXDAO <tombuxdao@gmail.com>
2025-04-15 10:39:30 +0100

    Exclude socket-server from Next.js build

--- next.config.js
+++ next.config.js
@@ -5,6 +5,14 @@ const nextConfig = {
       'cdn.discordapp.com'
     ],
   },
+  webpack: (config, { isServer }) => {
+    // Exclude socket-server directory from the build
+    config.watchOptions = {
+      ...config.watchOptions,
+      ignored: [...(config.watchOptions?.ignored || []), '**/socket-server/**'],
+    };
+    return config;
+  },
   // Add any other Next.js config options here
 };
 
